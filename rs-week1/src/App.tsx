1baa6a28dca75254bee9c4325a8a8a111b837bbf
Syderi <syderi@yahoo.com>
2023-03-06 15:44:05 +0300

    fix: add routing

--- rs-week1/src/App.tsx
+++ rs-week1/src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css';
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { HomePage } from './assets/components/pages/Home';
-import { Header } from './assets/components/Header';
-import { AboutUS } from './assets/components/pages/About';
-import { NotFound404 } from './assets/components/pages/Page404';
-
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { HomePage } from './components/pages/Home';
+import { Header } from './components/Header';
+import { AboutUS } from './components/pages/About';
+import { NotFound404 } from './components/pages/Page404';
 
 function App() {
   return (
@@ -15,6 +14,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutUS />} />
         <Route path="*" element={<NotFound404 />} />
+        {/* <Navigate to="/404" replace /> */}
       </Routes>
     </>
   );
--- rs-week1/src/components/Header.tsx
+++ rs-week1/src/components/Header.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export function Header() {
+  const location = useLocation();
+  const currentPath = location.pathname;
   return (
     <>
       <div>
         <Link to="/">Home Page</Link>
         <Link to="/about">About US</Link>
         <Link to="/404">Page 404</Link>
+        <h6>Current Page: {currentPath}</h6>
       </div>
     </>
   );

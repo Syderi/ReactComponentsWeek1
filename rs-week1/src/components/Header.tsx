8327de3ca209097331aa0e078c46ac03d298e2bf
Syderi <syderi@yahoo.com>
2023-03-07 13:24:49 +0300

    fix: add card fetch

--- rs-week1/src/components/Header.tsx
+++ rs-week1/src/components/Header.tsx
@@ -1,57 +1,75 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
-function Header() {
-  const location = useLocation();
-  const currentPath = location.pathname;
+type IHeaderProps = Record<string, never>;
 
-  const [searchInput, setSearchInput] = useState<string>('');
+interface IHeaderState {
+  searchInput: string;
+}
 
-  useEffect(() => {
+export class Header extends React.Component<IHeaderProps, IHeaderState> {
+  constructor(props: IHeaderProps) {
+    super(props);
+    this.state = {
+      searchInput: '',
+    };
+  }
+
+  componentDidMount() {
     const searchTerm = localStorage.getItem('searchTerm');
     if (searchTerm) {
-      setSearchInput(searchTerm);
+      this.setState({ searchInput: searchTerm });
     }
-  }, []);
+  }
 
-  useEffect(() => {
-    localStorage.setItem('searchTerm', searchInput);
-  }, [searchInput]);
+  componentDidUpdate(_: IHeaderProps, prevState: IHeaderState) {
+    const { searchInput } = this.state;
+    if (searchInput !== prevState.searchInput) {
+      localStorage.setItem('searchTerm', searchInput);
+    }
+  }
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchInput(event.target.value);
+  handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    this.setState({ searchInput: value });
   };
 
-  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
   };
 
-  return (
-    <>
-      <header className="header">
-        <nav className="navigation">
-          <div className="navigation-search">
-            <form onSubmit={handleFormSubmit}>
-              <input
-                type="text"
-                placeholder="Search..."
-                value={searchInput}
-                onChange={handleInputChange}
-              />
-              <button type="submit">Search</button>
-            </form>
-          </div>
-          <div className="navigation-links">
-            <Link to="/">Home Page</Link>
-            <Link to="/about">About US</Link>
-            <Link to="/404">Page 404</Link>
-          </div>
-        </nav>
-        <h6 className="current-page">Current Page: {currentPath}</h6>
-      </header>
-    </>
-  );
+  render() {
+    const { searchInput } = this.state;
+    // const { location } = this.props;
+    // const currentPath = location.pathname;
+
+    return (
+      <>
+        <header className="header">
+          <nav className="navigation">
+            <div className="navigation-search">
+              <form onSubmit={this.handleFormSubmit}>
+                <input
+                  type="text"
+                  placeholder="Search..."
+                  value={searchInput}
+                  onChange={this.handleInputChange}
+                />
+                <button type="submit">Search</button>
+              </form>
+            </div>
+            <div className="navigation-links">
+              <Link to="/">Home Page</Link>
+              <Link to="/about">About US</Link>
+              <Link to="/404">Page 404</Link>
+            </div>
+          </nav>
+          {/* <h6 className="current-page">Current Page: {currentPath}</h6> */}
+        </header>
+      </>
+    );
+  }
 }
 
 export default Header;
--- rs-week1/src/components/pages/Home.tsx
+++ rs-week1/src/components/pages/Home.tsx
@@ -1,17 +1,46 @@
-import products from '../dataProducts/products';
+// import products from '../dataProducts/products';
 import ProductCard from '../ProductCard';
 import React, { Component } from 'react';
 import './Home.css';
+import { IProduct } from 'components/types/interface';
 
-class HomePage extends Component {
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+// interface IHeaderProps {}
+
+interface IState {
+  products: IProduct[];
+}
+
+class HomePage extends Component<Record<string, never>, IState> {
+  state: IState = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    try {
+      const response = await fetch('https://dummyjson.com/products?limit=12');
+      const resProducts = await response.json();
+      // console.error('resProducts', resProducts);
+      const products = resProducts.products;
+      // console.error('products', products);
+      this.setState({ products });
+    } catch (error) {
+      console.error('Error fetching products:', error);
+    }
+  }
+
   render() {
+    const { products } = this.state;
+    console.log('products', products);
     return (
       <>
         <h3>Home page</h3>
         <div className="product-cards-container">
-          {products.map((product) => (
-            <ProductCard product={product} key={product.id} />
-          ))}
+          {products.length ? (
+            products.map((product) => <ProductCard product={product} key={product.id} />)
+          ) : (
+            <p>Loading ...</p>
+          )}
         </div>
       </>
     );

7d8b73240254bf34fe63c8b78536acd8e69bf5f9
Syderi <syderi@yahoo.com>
2023-03-15 14:11:52 +0300

    fix: test

--- rs-week1/src/__tests__/Home.test.tsx
+++ rs-week1/src/__tests__/Home.test.tsx
@@ -3,14 +3,13 @@ import React from 'react';
 import Home from '../components/pages/Home';
 import { render, fireEvent, screen } from '@testing-library/react';
 import { fn } from 'jest-mock';
-import '@testing-library/jest-dom'; // import the jest-dom library
 
 describe('<Home />', () => {
   test('handles search input change and form submission', () => {
     const onChangeNamePage = fn();
     const { getByPlaceholderText } = render(<Home onChangeNamePage={onChangeNamePage} />);
     const searchInput = getByPlaceholderText('Search...') as HTMLInputElement;
-    expect(searchInput).toBeTruthy();
+    expect(searchInput).toBeInTheDocument();
 
     const searchButton = screen.getByTestId('search-button');
     expect(searchButton).toBeInTheDocument();

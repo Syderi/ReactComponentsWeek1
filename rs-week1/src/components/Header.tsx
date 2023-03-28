d3d703c9710a111655c49cd27199bf05ed6b5c3c
Syderi <syderi@yahoo.com>
2023-03-28 13:08:42 +0300

    fix: Header

--- rs-week1/src/components/Header.tsx
+++ rs-week1/src/components/Header.tsx
@@ -6,29 +6,21 @@ interface IHeaderProps {
   namePageTerm: string;
 }
 
-export class Header extends React.Component<IHeaderProps> {
-  constructor(props: IHeaderProps) {
-    super(props);
-  }
-
-  render() {
-    const { namePageTerm } = this.props;
-
-    return (
-      <>
-        <header className="header">
-          <nav className="navigation">
-            <div className="navigation-links">
-              <NavLink to="/">Home Page</NavLink>
-              <NavLink to="/about">About US</NavLink>
-              <NavLink to="/form">Form Page</NavLink>
-            </div>
-            <h6 className="current-page">Current Page: {namePageTerm}</h6>
-          </nav>
-        </header>
-      </>
-    );
-  }
+function Header({ namePageTerm }: IHeaderProps) {
+  return (
+    <>
+      <header className="header">
+        <nav className="navigation">
+          <div className="navigation-links">
+            <NavLink to="/">Home Page</NavLink>
+            <NavLink to="/about">About US</NavLink>
+            <NavLink to="/form">Form Page</NavLink>
+          </div>
+          <h6 className="current-page">Current Page: {namePageTerm}</h6>
+        </nav>
+      </header>
+    </>
+  );
 }
 
 export default Header;

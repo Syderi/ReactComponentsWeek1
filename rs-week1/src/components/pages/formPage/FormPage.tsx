301c76de5ddc73aa27233562995b0416e351ee62
Syderi <syderi@yahoo.com>
2023-03-28 14:04:44 +0300

    fix: FormPage

--- rs-week1/src/components/pages/formPage/FormPage.tsx
+++ rs-week1/src/components/pages/formPage/FormPage.tsx
@@ -1,6 +1,6 @@
 import './FormPage.css';
 import { IFormCard } from 'components/types/interface';
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import FormCard from './FormCard';
 import FormInput from './FormInput';
 
@@ -8,49 +8,32 @@ interface IFormPageProps {
   onChangeNamePage: (namePage: string) => void;
 }
 
-interface IFormPageState {
-  products: IFormCard[];
-}
+function FormPage({ onChangeNamePage }: IFormPageProps) {
+  useEffect(() => {
+    onChangeNamePage('Form Page');
+  });
 
-class FormPage extends Component<IFormPageProps, IFormPageState> {
-  constructor(props: IFormPageProps) {
-    super(props);
-    this.state = {
-      products: [],
-    };
-  }
+  const [products, setProducts] = useState<IFormCard[]>([]);
 
-  componentDidMount() {
-    this.props.onChangeNamePage('Form Page');
-  }
-
-  onChangeProduct = (newProduct: IFormCard) => {
-    const { products } = this.state;
-    const newProducts = [...products, newProduct];
-
-    this.setState({
-      products: newProducts,
-    });
+  const onChangeProduct = (newProduct: IFormCard) => {
+    setProducts([...products, newProduct]);
   };
 
-  render() {
-    const { products } = this.state;
-    return (
-      <div className="form-page">
-        <h3>Form page</h3>
-        <FormInput onChangeProduct={this.onChangeProduct} />
-        <div className="form-cards-container">
-          {products.length ? (
-            products.map((product) => {
-              return <FormCard key={product.id} product={{ ...product }} />;
-            })
-          ) : (
-            <div>No products...</div>
-          )}
-        </div>
+  return (
+    <div className="form-page">
+      <h3>Form page</h3>
+      <FormInput onChangeProduct={onChangeProduct} />
+      <div className="form-cards-container">
+        {products.length ? (
+          products.map((product) => {
+            return <FormCard key={product.id} product={{ ...product }} />;
+          })
+        ) : (
+          <div>No products...</div>
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default FormPage;

cb197fa97fe979fc766e7436d85866a3bb21be69
Syderi <syderi@yahoo.com>
2023-03-28 13:00:45 +0300

    fix: ProductCard

--- rs-week1/src/components/ProductCard.tsx
+++ rs-week1/src/components/ProductCard.tsx
@@ -1,62 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { IProductProps } from './types/interface';
 import './ProductCard.css';
 import defaultPic from '../assets/img/default.png';
 
-interface IProductCardState {
-  thumbnailError: boolean;
-}
+function ProductCard({ product }: IProductProps) {
+  const [thumbnailError, seThumbnailError] = useState(false);
 
-class ProductCard extends Component<IProductProps, IProductCardState> {
-  constructor(props: IProductProps) {
-    super(props);
-    this.state = {
-      thumbnailError: false,
-    };
-  }
-
-  handleThumbnailError = () => {
-    this.setState({ thumbnailError: true });
+  const handleThumbnailError = () => {
+    seThumbnailError(true);
   };
 
-  render() {
-    const {
-      id,
-      title,
-      description,
-      price,
-      discountPercentage,
-      rating,
-      stock,
-      brand,
-      category,
-      thumbnail,
-    } = this.props.product;
+  const {
+    id,
+    title,
+    description,
+    price,
+    discountPercentage,
+    rating,
+    stock,
+    brand,
+    category,
+    thumbnail,
+  } = product;
 
-    const discountedPrice = price - (price * discountPercentage) / 100;
+  const discountedPrice = price - (price * discountPercentage) / 100;
 
-    return (
-      <div data-testid={`product-card${id}`} className="product-card" id={`product-card${id}`}>
-        <div className="product-card__image">
-          <img
-            src={this.state.thumbnailError ? defaultPic : thumbnail}
-            alt={title}
-            onError={this.handleThumbnailError}
-          />
-        </div>
-        <div className="product-card__info">
-          <h2>{title}</h2>
-          <p>{description}</p>
-          <p>Price: ${discountedPrice}</p>
-          <p>Rating: {rating}</p>
-          <p>Stock: {stock}</p>
-          <p>Brand: {brand}</p>
-          <p>Category: {category}</p>
-        </div>
-        <button className="product-card__button">Add to cart</button>
+  return (
+    <div data-testid={`product-card${id}`} className="product-card" id={`product-card${id}`}>
+      <div className="product-card__image">
+        <img
+          src={thumbnailError ? defaultPic : thumbnail}
+          alt={title}
+          onError={handleThumbnailError}
+        />
       </div>
-    );
-  }
+      <div className="product-card__info">
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <p>Price: ${discountedPrice}</p>
+        <p>Rating: {rating}</p>
+        <p>Stock: {stock}</p>
+        <p>Brand: {brand}</p>
+        <p>Category: {category}</p>
+      </div>
+      <button className="product-card__button">Add to cart</button>
+    </div>
+  );
 }
 
 export default ProductCard;

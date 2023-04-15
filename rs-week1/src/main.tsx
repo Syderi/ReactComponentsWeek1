b2877d36013b4f129f41095a09a315eee3bd6f2c
Syderi <syderi@yahoo.com>
2023-04-15 15:10:34 +0300

    fix: main

--- rs-week1/src/main.tsx
+++ rs-week1/src/main.tsx
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root') as Element, <App />);
+// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+ReactDOM.hydrateRoot(document.getElementById('root') as Element, <App />);

367b4a3fa94f3c2239c91c5fa19b7ea17affc5e5
Syderi <syderi@yahoo.com>
2023-04-16 22:44:12 +0300

    fix: packege.json

--- rs-week1/src/App.tsx
+++ rs-week1/src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
+import { Route, Routes } from 'react-router-dom';
 import Header from './components/header/Header';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
@@ -11,17 +9,13 @@ import Page404 from './pages/Page404/Page404';
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Provider store={store}>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/form" element={<FormPage />} />
-            <Route path="*" element={<Page404 />} />
-          </Routes>
-        </Provider>
-      </BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/form" element={<FormPage />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
     </>
   );
 }
--- rs-week1/src/main.tsx
+++ rs-week1/src/main.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 import App from './App';
 
-ReactDOM.hydrateRoot(document.getElementById('root') as Element, <App />);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);

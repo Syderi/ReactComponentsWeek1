884dc3460d52525a31db84cbf797dcfe230ecd94
Syderi <syderi@yahoo.com>
2023-04-16 21:34:35 +0300

    fix: entry-server

---
+++ rs-week1/src/entry-server.tsx
@@ -0,0 +1,35 @@
+import { configureStore } from '@reduxjs/toolkit';
+import App from './App';
+import {
+  PipeableStream,
+  renderToPipeableStream,
+  RenderToPipeableStreamOptions,
+} from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom/server';
+import { productApi } from './store/Api/Api';
+import { formSlice, reducer as formReducer } from './store/redusers/formSlice';
+import { stateSlice, reducer as searchReducer } from './store/redusers/searchSlice';
+import { Provider } from 'react-redux';
+import React from 'react';
+
+export function renderApp(url: string, options: RenderToPipeableStreamOptions): PipeableStream {
+  const store = configureStore({
+    reducer: {
+      [productApi.reducerPath]: productApi.reducer,
+      [formSlice.name]: formReducer,
+      [stateSlice.name]: searchReducer,
+    },
+    middleware: [],
+  });
+
+  const stream = renderToPipeableStream(
+    <Provider store={store}>
+      <StaticRouter location={url}>
+        <App />
+      </StaticRouter>
+    </Provider>,
+
+    options
+  );
+  return stream;
+}
--- rs-week1/vite.config.ts
+++ rs-week1/vite.config.ts
@@ -7,6 +7,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  optimizeDeps: { include: ['react/jsx-dev-runtime'] },
+  build: {
+    minify: false,
+  },
   test: {
     globals: true,
     environment: 'jsdom',

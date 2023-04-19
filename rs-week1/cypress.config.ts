1267131d36f4fcd7a3acb8cb13d98250c8652a9b
Syderi <syderi@yahoo.com>
2023-04-19 13:37:13 +0300

    fix: coverage

--- rs-week1/cypress.config.ts
+++ rs-week1/cypress.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'cypress';
+import registerCodeCoverageTasks from '@cypress/code-coverage/task';
 
 export default defineConfig({
+  env: {
+    codeCoverage: {
+      exclude: 'cypress/**/*.*',
+    },
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      registerCodeCoverageTasks(on, config);
+      return config;
     },
     baseUrl: 'http://localhost:3000',
   },
